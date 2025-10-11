e9d6a7d10fe6325eee93ca518456812102c681f2
DHARSHINI PRIYA A <dharshinipriya.a426@gmail.com>
2025-10-11 15:41:34 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top"
-      style={{ backgroundColor: 'rgba(10, 10, 10, 0.9)', backdropFilter: 'blur(5px)' }}>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark fixed-top"
+      style={{ backgroundColor: 'rgba(10, 10, 10, 0.9)', backdropFilter: 'blur(5px)' }}
+    >
       <div className="container">
         <a className="navbar-brand" href="#">
-          <img src="brand.png" width="90px" alt="Brand" />
+          {/* make brand image responsive instead of fixed width */}
+          <img src="brand.png" alt="Brand" className="brand-img img-fluid" style={{ maxWidth: 90 }} />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
@@ -22,6 +33,7 @@ const Navbar = () => {
             <li className="nav-item">
               <a className="nav-link" href="#skills">Skills</a>
             </li>
+           
             <li className="nav-item">
               <a className="nav-link" href="#education">Education</a>
             </li>
