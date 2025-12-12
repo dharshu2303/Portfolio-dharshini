2213fa5af19064fa47f87c268d2e28cb0be8a322
DHARSHINI PRIYA A <dharshinipriya.a426@gmail.com>
2025-12-12 16:03:27 +0530

    Add new award achievement to Awards component

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -8,6 +8,13 @@ const Awards = () => {
         <div className="row">
           <div className="col-md-6 col-lg-3 mb-4">
             <div className="card h-100 text-center p-4">
+              <i className="fas fa-user-graduate fa-3x mb-3 neon-pink"></i>
+              <h4>Ranked Top 5% in NPTEL Cloud Computing Certification Course</h4>
+              <p className="text-muted">M Kumarasamy College of Engineering,Karur</p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-3 mb-4">
+            <div className="card h-100 text-center p-4">
               <i className="fas fa-trophy fa-3x mb-3 neon-green"></i>
               <h4>Awarded Student of the Month at college.</h4>
               <p className="text-muted">M Kumarasamy College of Engineering,Karur</p>
